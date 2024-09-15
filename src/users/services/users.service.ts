37e5c16db142834d656ed2937f7edc2580b8f8c8
candelaria_sobol <csobol@mobydigital.com>
2024-09-15 14:45:21 -0300

    connect to business ms and call the findall endpoint with pagination

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,11 +1,25 @@
-import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { UsersRepository } from '../repositories/users.repository';
-import { CreateUserDto } from '../../auth/dto/create-user.dto';
-import * as bcrypt from 'bcrypt';
+import { InjectConnection } from '@nestjs/mongoose';
+import { ClientProxy } from '@nestjs/microservices';
+import { Connection } from 'mongoose';
+import { PaginationDto } from '../dto/pagination.dto';
+import { firstValueFrom } from 'rxjs';
+
 
 @Injectable()
 export class UsersService {
 
-    constructor(private readonly usersRepository: UsersRepository) { }
+    constructor(
+        @Inject('BUSINESS_SERVICE') private readonly businessClient: ClientProxy,
+        @InjectConnection() private readonly connection: Connection,
+        private readonly usersRepository: UsersRepository,
+      ) {}
+
+      async findAll(paginationDto: PaginationDto) {
+        return firstValueFrom(
+          this.businessClient.send({ cmd: 'get user pagination' }, paginationDto),
+        );
+      }
 
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,10 +5,27 @@ import { DatabaseModule } from 'src/database/database.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserDocument, UserSchema } from '../auth/schemas/user.schema';
 import { UsersRepository } from './repositories/users.repository';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [DatabaseModule,
     MongooseModule.forFeature([{ name: UserDocument.name, schema: UserSchema }]),
+    ConfigModule.forRoot(),
+    ClientsModule.registerAsync([
+      {
+        name: 'BUSINESS_SERVICE',
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.TCP,
+          options: {
+            host: configService.get<string>('BUSINESS_SERVICE_HOST'),
+            port: parseInt(configService.get<string>('BUSINESS_SERVICE_PORT'), 10),
+          },
+        }),
+      },
+    ]),
   ],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
